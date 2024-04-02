af630494ade9882bfccef4e1a78808e6a9eb1da0
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-04-02 13:02:05 +0400

    commit 001 (Saba) | ts router bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Note from './components/Note';
 import CreateNote from './components/CreateNote';
 // import EditComponent from './EditComponent';
 
-function App(): JSX.Element {
+function App() {
   return (
-    <Router>
-      <Route path="/" exact={true} component={Note} />
-      <Route path="/create" component={CreateNote} />
+    <Routes>
+      <Route path="/" element={<Note />} />
+      <Route path="/create" element={<CreateNote />} />
       {/* <Route path="/edit/:id" component={EditComponent} /> */}
-    </Router>
+    </Routes>
   );
 }
 
